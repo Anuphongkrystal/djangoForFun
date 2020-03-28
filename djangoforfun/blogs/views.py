963eab7e43eaddcb7a4b0fe6ef39354ba7d22630
Anuphongkrystal <Anuphongkrystal@outlook.com>
2020-03-28 10:07:27 -0700

    For loop

--- djangoforfun/blogs/views.py
+++ djangoforfun/blogs/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 
 def hello(request): #เปรียบสเมือนเราสร้าง controler ขึ้นมา
     #return HttpResponse("<h2>Hello World</h2>")
-
-    return render(request,'index.html',{'name':'Onlyone','author':'Levi'})
+    tags = ['dog','Levi','cat','kookkai']
+    return render(request,'index.html',
+    {
+    'name':'Onlyone',
+    'author':'Levi',
+    'tags':tags
+    })
