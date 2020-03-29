a537522e743879443681c3f5ecd8b24d116e7760
Anuphongkrystal <Anuphongkrystal@outlook.com>
2020-03-29 08:26:01 -0700

    query

--- djangoforfun/blogs/views.py
+++ djangoforfun/blogs/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 # Create your views here.
 
 def hello(request): #เปรียบสเมือนเราสร้าง controler ขึ้นมา
+
+    #quer data
+    data = Post.objects.all()
+    return render(request,'index.html',{'posts':data})
     #return HttpResponse("<h2>Hello World</h2>")
-    tags = ['dog','Levi','cat','kookkai']
-    rating = 4
-    return render(request,'index.html',
-    {
-    'name':'Onlyone',
-    'author':'Levi',
-    'tags':tags,
-    'rating':rating
-    })
+    '''    tags = ['dog','Levi','cat','kookkai']
+        rating = 4
+        return render(request,'index.html',
+        {
+        'name':'Onlyone',
+        'author':'Levi',
+        'tags':tags,
+        'rating':rating
+        })
+    '''
 
 def page1(request):
     return render(request,'page1.html')
